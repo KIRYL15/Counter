8e404bd5879db5409e91faec3c0755a172319b38
Kirill Pravdin <kirill-pravdin@mail.ru>
2021-12-19 23:51:55 +0300

    locolstorage

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,40 @@ import './App.css';
 import {Table} from "./Components/Counter";
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [inc, setInc] = useState(0)
+
+    // const setLocalStorageHandler = () => {
+    //     localStorage.setItem('counterValue', JSON.stringify(inc))
+    //     localStorage.setItem('counterValue+1', JSON.stringify(inc+1))
+    //
+    // }
+    // const getLocalStorageHandler = () => {
+    //     let valueAsString=localStorage.getItem('counterValue')
+    //     if(valueAsString){
+    //     let newValue=JSON.parse(valueAsString)
+    //         setInc(newValue)
+    //     }
+    // }
+    // const clearLocalStorageHandler = () => {
+    //   localStorage.clear()
+    //     setInc(0)
+    // }
+    // const removeLocalStorageHandler = () => {
+    //   localStorage.removeItem('counterValue+1')
+    //     //setInc(0)
+    // }
+
+
     return (
+
         <div className="App">
+            {/*<button onClick={setLocalStorageHandler}>setLocalStorage</button> запись
+            <button onClick={getLocalStorageHandler}>getLocalStorage</button> добавление
+            <button onClick={clearLocalStorageHandler}>clearLocalStorage</button> очистка всех значений
+            <button onClick={removeLocalStorageHandler}>removeLocalStorage</button> очистка всех значений*/}
             <Table
-                count={count}
-                setCount={setCount}
+                inc={inc}
+                setInc={setInc}
             />
         </div>
     );
--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -1,27 +1,46 @@
-import React from "react";
+import React, {useEffect} from "react";
 import s from './Counter.module.css'
 
 export type TablePropsType = {
-    count: number
-    setCount: (count: number) => void
+    inc: number
+    setInc: (inc: number) => void
 }
 
 export const Table = (props: TablePropsType) => {
 
+    useEffect(() => {
+        let valueAsString = localStorage.getItem('counterValue')
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            props.setInc(newValue)
+        }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('counterValue', JSON.stringify(props.inc))
+    }, [props.inc])
+
+    let onButonClick = () => {
+        props.setInc(props.inc + 1)
+    }
+
     return (
         <div className={s.window}>
             <div className={s.table}>
-                <div className={props.count === 5 ? s.newNumberStyle : s.number}> {props.count} </div>
+                <div className={props.inc === 5 ? s.newNumberStyle : s.number}> {props.inc} </div>
                 <div className={s.buttonBlock}>
-                    <button className={props.count === 5 ? s.newIncStyle : s.incStyle} disabled={props.count === 5}
-                            onClick={() => props.setCount(props.count + 1)}>Inc
+
+
+                    <button className={props.inc === 5 ? s.newIncStyle : s.incStyle}
+                            disabled={props.inc === 5}
+                            onClick={onButonClick}>Inc
                     </button>
-                    <button className={props.count === 0 ? s.newResStyle : s.resStyle} disabled={props.count === 0}
-                            onClick={() => props.setCount(0)}>Reset
+                    <button className={props.inc === 0 ? s.newResStyle : s.resStyle}
+                            disabled={props.inc === 0}
+                            onClick={() => props.setInc(0)}>Reset
                     </button>
                 </div>
             </div>
         </div>
-
     )
-}
+};
